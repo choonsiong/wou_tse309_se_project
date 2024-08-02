2635d0179ff6e164f1023d5d8aa0f0b591875832
choonsiong <choonsiong.lee@gmail.com>
2024-08-02 22:05:49 +0800

    Complete home page

--- my-book-library-frontend/src/App.vue
+++ my-book-library-frontend/src/App.vue
@@ -1,14 +1,20 @@
 <template>
     <main-header></main-header>
+    <main-content></main-content>
+    <main-footer></main-footer>
 </template>
 
 <script>
 import MainHeader from "@/components/MainHeader.vue";
+import MainFooter from "@/components/MainFooter.vue";
+import MainContent from "@/components/MainContent.vue";
 
 export default {
     name: 'App',
     components: {
-        MainHeader
+        MainHeader,
+        MainContent,
+        MainFooter,
     }
 }
 </script>
