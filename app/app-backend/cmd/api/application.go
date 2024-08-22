a176c465f708d5222408815e50d3f28688f008ef
choonsiong <choonsiong.lee@gmail.com>
2024-08-23 03:09:47 +0800

    Add support for chi router and refactor http handlers

--- app/app-backend/cmd/api/application.go
+++ app/app-backend/cmd/api/application.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -16,23 +15,28 @@ type application struct {
 
 // start the web server
 func (app *application) serve() error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var payload struct {
-			Status  bool   `json:"status"`
-			Message string `json:"message"`
-		}
-		payload.Status = true
-		payload.Message = "Hello World"
-		out, err := json.MarshalIndent(payload, "", "\t")
-		if err != nil {
-			app.errorLog.Println(err)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(out)
-	})
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	var payload struct {
+	//		Status  bool   `json:"status"`
+	//		Message string `json:"message"`
+	//	}
+	//	payload.Status = true
+	//	payload.Message = "Hello World"
+	//	out, err := json.MarshalIndent(payload, "", "\t")
+	//	if err != nil {
+	//		app.errorLog.Println(err)
+	//	}
+	//	w.Header().Set("Content-Type", "application/json")
+	//	w.WriteHeader(http.StatusOK)
+	//	_, _ = w.Write(out)
+	//})
 
 	app.infoLog.Println("Listening on port ", app.cfg.port)
 
-	return http.ListenAndServe(fmt.Sprintf(":%d", app.cfg.port), nil)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", app.cfg.port),
+		Handler: app.routes(),
+	}
+
+	return srv.ListenAndServe()
 }
---
+++ app/app-backend/cmd/api/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// middlewares
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/users/login", app.Login)
+	mux.Post("/users/login", app.Login)
+
+	return mux
+}
