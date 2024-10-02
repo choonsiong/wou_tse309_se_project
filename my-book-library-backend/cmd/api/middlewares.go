85c7ef604a2c891ece80a327f9fcee6437a9f35e
choonsiong <choonsiong.lee@gmail.com>
2024-10-03 02:05:09 +0800

    Update error handling

--- my-book-library-backend/cmd/api/middlewares.go
+++ my-book-library-backend/cmd/api/middlewares.go
@@ -6,6 +6,7 @@ func (app *application) AuthenticateToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, err := app.models.Token.AuthenticateToken(r)
 		if err != nil {
+			app.errorLog.Println(err)
 			payload := jsonResponse{
 				Error:   true,
 				Message: err.Error(),
