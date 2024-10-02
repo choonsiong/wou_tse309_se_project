0b2e72f1c6d1e72d03bdfb09dd29afb7d254eeeb
choonsiong <choonsiong.lee@gmail.com>
2024-10-03 02:05:43 +0800

    Update all users page

--- my-book-library-frontend/src/components/AllUsers.vue
+++ my-book-library-frontend/src/components/AllUsers.vue
@@ -4,11 +4,39 @@
 
 <script>
 import Security from '@/security.js'
+import appEnvironment from '@/environment.js'
+import notie from 'notie'
 
 export default {
   name: 'AllUsers',
+  data() {
+    return {
+      users: []
+    }
+  },
   beforeMount() {
     Security.requireAdmin()
+
+    fetch(appEnvironment.apiURL() + '/admin/users/all', Security.requestOptions(""))
+      .then((resp) => resp.json())
+      .then((jsonResp) => {
+        if (jsonResp.error) {
+          //console.log('error: ' + jsonResp.message)
+          notie.alert({
+            type: 'error',
+            text: jsonResp.message,
+          })
+        } else {
+          //console.log(jsonResp)
+          this.users = jsonResp.data.users
+        }
+      })
+      .catch((err) => {
+        notie.alert({
+          type: 'error',
+          text: err
+        })
+      })
   },
 }
 </script>
