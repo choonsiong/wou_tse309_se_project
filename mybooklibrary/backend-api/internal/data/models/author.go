d1e36436aef47313c58aa701e56c9c6e5557ac41
choonsiong <choonsiong.lee@gmail.com>
2024-10-04 01:43:08 +0800

    Update book model

--- mybooklibrary/backend-api/internal/data/models/author.go
+++ mybooklibrary/backend-api/internal/data/models/author.go
@@ -1,10 +1,37 @@
 package models
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Author struct {
 	ID         int       `json:"id"`
 	AuthorName string    `json:"author_name"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+// All returns a list of all authors
+func (a *Author) All() ([]*Author, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), databaseTimeout)
+	defer cancel()
+
+	query := `SELECT id, author_name, created_at, updated_at  FROM authors ORDER BY author_name`
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var authors []*Author
+
+	for rows.Next() {
+		var author Author
+		err := rows.Scan(&author.ID, &author.AuthorName, &author.CreatedAt, &author.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		authors = append(authors, &author)
+	}
+	return authors, nil
 }
