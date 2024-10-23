d3551e95847e7c436598c0d476ab7bd8e25cb2af
choonsiong <choonsiong.lee@gmail.com>
2024-10-23 15:35:03 +0800

    Update book detail page

--- mybooklibrary/frontend-ui/src/components/BookDetail.vue
+++ mybooklibrary/frontend-ui/src/components/BookDetail.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="p-4 bg-green-100">
-    <p class="font-bold text-center text-3xl">{{ book.title }}</p>
-    <div class="m-2.5 overflow-hidden rounded-md h-80 flex justify-center items-center">
-      <img class="w-full h-full object-scale-down" :src="appEnvironment.imageURL() + '/' + book.slug + '.jpg'" alt="profile-picture" />
-    </div>
-    <p class="text-center font-light text-xl">{{ allAuthors }}</p>
-    <p class="mt-5 text-center font-bold text-2xl">{{ normalizedPublisherName }} ({{ book.publication_year }})</p>
-    <p class="mt-10 font-extralight">{{ book.description }}</p>
-    <div class="mt-10">
-      <div class="flex flex-wrap mx-3 pb-3 pt-2 px-1">
-        <span v-for="genre in book.genres" class="text-sm text-slate-600 font-medium me-2 mb-2 border-2 rounded-md p-1">{{ genre.genre_name.toLowerCase() }}</span>
+    <div class="w-[80%] mx-auto pt-10">
+      <p class="font-bold text-center text-3xl">{{ book.title }}</p>
+      <div class="m-2.5 overflow-hidden rounded-md h-80 flex justify-center items-center">
+        <img class="w-full h-full object-scale-down" :src="appEnvironment.imageURL() + '/' + book.slug + '.jpg'"
+             alt="profile-picture" />
+      </div>
+      <p class="mt-3 text-center font-light text-xl">{{ allAuthors }}</p>
+      <p class="mt-5 text-center font-bold text-2xl">{{ normalizedPublisherName }} ({{ book.publication_year }})</p>
+      <p class="mt-10 font-extralight">{{ book.description }}</p>
+      <div class="mt-10">
+        <div class="flex flex-wrap mx-3 pb-3 pt-2 px-1">
+          <span v-for="genre in book.genres"
+                class="text-sm text-slate-600 font-medium me-2 mb-2 border-2 rounded-md p-1">{{ genre.genre_name.toLowerCase()
+            }}</span>
+        </div>
+      </div>
+      <div class="text-center">
+        <button v-if="store.isLoggedIn" class="bg-green-400 p-3 rounded-xl font-bold text-white" @click="handleWriteReview">Write a Review</button>
       </div>
     </div>
   </div>
@@ -17,15 +25,19 @@
 <script>
 import { store } from '@/store.js'
 import appEnvironment from '@/environment.js'
+import router from '@/router/index.js'
 
 export default {
   name: 'BookDetail',
   data() {
     return {
-      book: store.book,
+      book: store.book
     }
   },
   computed: {
+    store() {
+      return store
+    },
     appEnvironment() {
       return appEnvironment
     },
@@ -45,15 +57,19 @@ export default {
     },
     normalizedPublisherName() {
       return this.capitalizedEachWord(store.book.publisher.publisher_name)
-    },
+    }
   },
   methods: {
     capitalizedEachWord(words) {
-      const arr = words.split(" ")
+      const arr = words.split(' ')
       for (let i = 0; i < arr.length; i++) {
         arr[i] = arr[i][0].toUpperCase() + arr[i].substring(1)
       }
-      return arr.join(" ")
+      return arr.join(' ')
+    },
+    handleWriteReview() {
+      store.book = this.book
+      router.push('/manage/reviews/new')
     }
   }
 }
