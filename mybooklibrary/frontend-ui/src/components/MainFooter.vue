b252fe9e410c2821dea06b5a36e89218924d3123
choonsiong <choonsiong.lee@gmail.com>
2024-10-31 01:45:21 +0800

    Update version to 1.5.2

--- mybooklibrary/frontend-ui/src/components/MainFooter.vue
+++ mybooklibrary/frontend-ui/src/components/MainFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-green-200 m-0 p-0 text-center">
         <p class="pt-5 font-bold">My Book Library</p>
-        <small class="text-[0.8rem] block mt-2 pb-3">v1.5.1<br/>COPYRIGHT &copy; {{ new Date().getFullYear() }} Lee Choon Siong</small>
+        <small class="text-[0.8rem] block mt-2 pb-3">v1.5.2<br/>COPYRIGHT &copy; {{ new Date().getFullYear() }} Lee Choon Siong</small>
     </footer>
 </template>
 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias culpa cumque cupiditate dicta et illum labore laboriosam, molestias possimus. Aliquam asperiores aspernatur autem, cupiditate debitis deserunt expedita ipsam iste libero, necessitatibus officiis praesentium sunt, vitae. A ab aliquid assumenda consectetur delectus deserunt eveniet facere illum in itaque labore laboriosam libero magnam magni minima necessitatibus neque perspiciatis porro, praesentium, quae quo ratione voluptate! Accusantium asperiores aspernatur corporis delectus, dolore eius esse eveniet exercitationem fuga harum iure laboriosam libero quae quidem quisquam reprehenderit repudiandae saepe, tempore temporibus ullam vitae voluptatem voluptatibus voluptatum? Ab, assumenda consequuntur deleniti dolore doloribus et illo odio tempore!
